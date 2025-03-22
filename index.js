f2667f52b72b4af7c3cec9ccc98561b0a5930546
Lakshay Kumar <lakshay.gla_cs21@gla.ac.in>
2025-03-22 11:34:01 +0530

    ADDED

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log(" Jai Hind! Welcome to Address Book System");
-//UC1
+//UC1 and UC2
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
         this.validateName(firstName, "First Name");
@@ -55,6 +55,63 @@ class Contact {
 try {
     let contact = new Contact("Amit", "Sharma", "Sector 15", "Noida", "Uttar Pradesh", "201301", "91 9876543210", "amit.sharma@gmail.com");
     console.log(contact.toString());
+} catch (error) {
+    console.error(error);
+}
+
+// UC2 : Ability to ensure Valid Contacts are added
+// Already Implemented in UC1.
+
+// UC3: Ability to Create Multiple Address Books and Add Contacts
+class AddressBookManager {
+    constructor() {
+        this.addressBooks = new Map(); // Map to store multiple address books
+    }
+
+    createAddressBook(name) {
+        if (this.addressBooks.has(name)) {
+            console.log(`Address Book '${name}' already exists.`);
+        } else {
+            this.addressBooks.set(name, []);
+            console.log(`Address Book '${name}' created successfully.`);
+        }
+    }
+
+    addContactToAddressBook(bookName, contact) {
+        if (this.addressBooks.has(bookName)) {
+            let addressBook = this.addressBooks.get(bookName);
+            addressBook.push(contact);
+            console.log(`Contact added to Address Book '${bookName}'.`);
+        } else {
+            console.log(`Address Book '${bookName}' does not exist.`);
+        }
+    }
+
+    viewAddressBook(bookName) {
+        if (this.addressBooks.has(bookName)) {
+            let addressBook = this.addressBooks.get(bookName);
+            console.log(`Address Book '${bookName}' contains:`);
+            addressBook.forEach(contact => console.log(contact.toString()));
+        } else {
+            console.log(`Address Book '${bookName}' does not exist.`);
+        }
+    }
+}
+
+// Example Usage
+let manager = new AddressBookManager();
+manager.createAddressBook("Family");
+manager.createAddressBook("Work");
+
+try {
+    let contact1 = new Contact("Ayush", "Srivastava", "Indira Nagar", "Raebareli", "Uttar Pradesh", "229001", "91 9876543210", "ayush@gmail.com");
+    manager.addContactToAddressBook("Family", contact1);
+
+    let contact2 = new Contact("Rahul", "Sharma", "Home base", "Anantnag", "Jammu Kashmir", "192101", "91 9876543211", "rahul@gmail.com");
+    manager.addContactToAddressBook("Work", contact2);
+
+    manager.viewAddressBook("Family");
+    manager.viewAddressBook("Work");
 } catch (error) {
     console.error(error);
 }
